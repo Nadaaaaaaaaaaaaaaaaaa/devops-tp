d0826b9744403352541f3228bd885216477d386e
Nada Joobeur <110560833+Nadaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2025-03-07 22:37:34 +0100

    Update smoke_test.sh
    ajouter un délai avant d'exécuter le test

--- smoke_test.sh
+++ smoke_test.sh
@@ -3,6 +3,10 @@
 # URL de l'application déployée
 APP_URL="http://localhost:5000"
 
+# Attendre que l'application soit prête
+echo "Waiting for the app to start..."
+sleep 10  # Attendre 10 secondes (ajustez selon les besoins)
+
 # Effectuer une requête GET à l'application
 response=$(curl -s -o /dev/null -w "%{http_code}" $APP_URL)
 
